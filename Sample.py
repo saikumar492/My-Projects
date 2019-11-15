b20104a99a235951644cf51b389075c374192a47
saikumar492 <saikumar4559.sk@gmail.com>
2019-11-15 15:06:33 +0530

    second Change

--- Sample.py
+++ Sample.py
@@ -3,7 +3,7 @@ for i in range(1,10):
         print("*", end='')
     print("")
     
-for i in range(10,1,-1):
+for i in range(10,0,-1):
     for j in range(i):
         print("*",end='')
     print('')
