7f8ace2dc6644348e8420c7f6fded88f691ad6b6
saikumar492 <saikumar4559.sk@gmail.com>
2019-11-15 15:04:28 +0530

    Change

--- Sample.py
+++ Sample.py
@@ -2,3 +2,8 @@ for i in range(1,10):
     for j in range(i):
         print("*", end='')
     print("")
+    
+for i in range(10,1,-1):
+    for j in range(i):
+        print("*",end='')
+    print('')
